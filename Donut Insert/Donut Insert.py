cc716e8ba18836b580fb66338ce3779d8210a000
chrisg.local@gmail.com <chrisg.local@gmail.com>
2022-05-27 13:35:05 +1000

    Basic Donut Shape Create the basic donut shape using two cylinders.

--- Donut Insert/Donut Insert.py
+++ Donut Insert/Donut Insert.py
@@ -9,8 +9,25 @@ from math import sin,cos
 SEGMENTS = 48
 
 def donut_insert():
+    outer_diameter = 100
+    insert_diameter = 20
+    wall = 3
+    height = 20
+    solid_bottom = True
 
-    return (cube([10,10,10]))
+    inner_diameter = outer_diameter - insert_diameter - (wall * 2)
+    donut = (cylinder(d=outer_diameter,h=height))
+    inner_hole = (cylinder(d=inner_diameter, h=height + wall * 2))
+
+    if(solid_bottom):
+            z_offset = wall
+    else:
+            z_offset = -wall
+
+    inner_hole = translate([0, 0, z_offset])(cylinder(d=inner_diameter, h=height + wall * 2))
+    donut -= inner_hole
+
+    return donut
 
 if __name__ == '__main__':
     out_dir = sys.argv[1] if len(sys.argv) > 1 else None
