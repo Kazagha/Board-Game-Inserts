367cc90967ae0fd00733c51745815d23dfb1b0d9
chrisg.local@gmail.com <chrisg.local@gmail.com>
2022-05-27 19:44:03 +1000

    Position Multiple Stacks Add logic to evenly space any number of stack. Replace the static diameter with a calculated diameter based on the size and number of the insert stacks.

--- Donut Insert/Donut Insert.py
+++ Donut Insert/Donut Insert.py
@@ -4,13 +4,13 @@ import sys
 from solid import scad_render_to_file
 from solid.objects import cube, cylinder, difference, translate, union, hull, rotate
 from solid.utils import right,left,up,down,minkowski
-from math import sin,cos
+from math import sin,cos,pi
 
 SEGMENTS = 48
 wall = 3
 
-def create_donut(diameter, insert_diameter, height, solid_bottom):
-    inner_diameter = diameter - insert_diameter - (wall * 2)
+def create_donut(diameter, outer_wall, height, solid_bottom):
+    inner_diameter = diameter - (outer_wall * 2)
     donut = (cylinder(d=diameter,h=height))
 
     if(solid_bottom):
@@ -23,17 +23,27 @@ def create_donut(diameter, insert_diameter, height, solid_bottom):
     return donut
 
 def donut_insert():
-    outer_diameter = 100
     insert_diameter = 20
-    outer_wall_thickness = insert_diameter - (wall * 2)
     height = 20
-    solid_bottom = True
+    number_of_stacks = 5
+    solid_bottom = False
+    """ Calculated Variables """
+    # Determine the diameter of the circle
+    #  - Calc circumference, number of stack with extra space
+    #  - Calc diameter, circumference / pi
+    outer_diameter = insert_diameter * number_of_stacks * 1.75 / pi
+    # Determine the thickness of the outer ring
+    outer_wall_thickness = insert_diameter - (wall * 2)
 
-    donut = create_donut(outer_diameter, insert_diameter, height, solid_bottom)
-    stack = (cylinder(d=insert_diameter,h=height))
-    stack = translate([outer_diameter/2 - outer_wall_thickness / 2, 0, wall])(stack)
-    donut -= stack
-    
+    donut = create_donut(outer_diameter, outer_wall_thickness, height, solid_bottom)
+    rotate_step = 360 / number_of_stacks
+    stack = (cylinder(d=insert_diameter, h=height))
+    stack = translate([outer_diameter / 2 - outer_wall_thickness / 2, 0, wall])(stack)
+    for i in range(number_of_stacks):
+        #stack = translate([outer_diameter / 2 - outer_wall_thickness / 2, 0, wall])(stack)
+
+        donut -= rotate([0,0,i * rotate_step])(stack)
+
     return donut
 
 if __name__ == '__main__':
