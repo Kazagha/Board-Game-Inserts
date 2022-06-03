e85b3eb8a66b66aa37db7e022abe9b866b8ee822
chrisg.local@gmail.com <chrisg.local@gmail.com>
2022-06-03 15:02:40 +1000

    Adjust Insert Size Change the insert size based on measurements from the Wingspan board game

--- Donut Insert/Donut Insert.py
+++ Donut Insert/Donut Insert.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 import sys
 
-from solid import scad_render_to_file
-from solid.objects import cube, cylinder, difference, translate, union, hull, rotate
+from solid import scad_render_to_file,color
+from solid.objects import cube, cylinder, difference, translate, union, hull, rotate,circle
 from solid.utils import right,left,up,down,minkowski,rotate_extrude,distribute_in_grid
 from math import sin,cos,pi
 
@@ -24,7 +24,7 @@ def create_donut(diameter, outer_wall, height, solid_bottom):
 
 def donut_insert():
     insert_diameter = 20
-    height = 20
+    height = 22 + wall 
     number_of_stacks = 5
     solid_bottom = False
     """ Calculated Variables """
@@ -42,11 +42,18 @@ def donut_insert():
     for i in range(number_of_stacks):
         donut -= rotate([0,0,i * rotate_step])(stack)
 
-    lid = (cylinder(d=outer_diameter + wall, h = wall * 2))
-    lid -= (translate([0,0,wall]))(cylinder(d=outer_diameter, h = wall+1))
+    lid = (cylinder(d=outer_diameter + wall + 1, h = wall * 2))
+    lid -= (translate([0,0,wall]))(cylinder(d=outer_diameter + 1, h = wall+1))
     lid = (translate([75,0,0]))(lid)
+    # Arrange Objects
     all_objects = [donut, lid]
-    return distribute_in_grid(all_objects,max_bounding_box=[5,5],rows_and_cols=[len(all_objects),1],)
+    all_objects = distribute_in_grid(all_objects,max_bounding_box=[5,5],rows_and_cols=[len(all_objects),1],)
+
+    #sub = (circle(d=50))
+    #sub = (translate([outer_diameter,0,0]))(sub)
+    #sub = rotate_extrude()(sub)
+
+    return all_objects
 
 if __name__ == '__main__':
     out_dir = sys.argv[1] if len(sys.argv) > 1 else None
