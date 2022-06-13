13d14935fc8a593d179885b204b9a0a29b47b17e
chrisg.local@gmail.com <chrisg.local@gmail.com>
2022-06-13 13:53:03 +1000

    Tokens and Box: Add Comments Add comments to the existing code

--- Tokens and Box/Tokens and Box.py
+++ Tokens and Box/Tokens and Box.py
@@ -10,24 +10,33 @@ SEGMENTS = 48
 wall = 3
 
 def tokens_and_insert():
-    height = 22
+    height = 44 + wall
     token_diameter = 20
     number_of_tokens = 5
-    length = 100
-    # Calculated Variables
-    width = (token_diameter + wall) * number_of_tokens + (wall * 2)
 
-    token = (cylinder(d=token_diameter, h=height + wall))
+    # Calculated Variables
+    width = (token_diameter + wall) * number_of_tokens + (wall)
+    main_box_length = width / 2
+
+    # Create the token inserts
+    token = (cylinder(d=token_diameter, h=height))
     all_tokens = (cube([0,0,0]))
     for i in range(number_of_tokens):
         all_tokens += translate([((token_diameter + wall) * i  ),0,0])(token)
 
-    token_box = (cube([width,token_diameter, height + wall]))
+    # Create the token section of the box
+    token_box = (cube([width,token_diameter, height]))
     token_box -= translate([(token_diameter / 2) + wall, (token_diameter / 2) - wall, wall])(all_tokens)
 
-    #distribute_in_grid(all_objects, max_bounding_box=[5, 5], rows_and_cols=[len(all_objects), 1], )
+    # Create the main box with an insert
+    main_box = (cube([width, main_box_length, height]))
+    insert = translate([wall,wall,wall])(cube([width - wall * 2, width / 2 - wall * 2, height]))
+    main_box -= insert
 
-    return token_box
+    # Add the main box and token section together 
+    main_box += translate([0,-token_diameter + wall,0])(token_box)
+
+    return main_box
 
 if __name__ == '__main__':
     out_dir = sys.argv[1] if len(sys.argv) > 1 else None
