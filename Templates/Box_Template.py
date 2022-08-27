5bc6ef735b862a32ec7b6474ca76a20a746e199a
chrisg.local@gmail.com <chrisg.local@gmail.com>
2022-08-27 13:51:09 +1000

    Box_Template: Initial commit An attempt to create helper functions that make it easire to create specific box shapes and inserts without needing to repeat steps

---
+++ Templates/Box_Template.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+import sys
+
+from solid import scad_render_to_file,color
+from solid.objects import cube, cylinder, difference, translate, union, hull, rotate,circle,sphere
+from solid.utils import right,left,up,down,minkowski,rotate_extrude,distribute_in_grid,linear_extrude
+from math import sin,cos,pi
+
+SEGMENTS = 48
+wall = 3
+
+#def Create_Box(dimensions, minkowski=False, inserts=[1], minkowski_insert=False):
+def Create_Box(dimensions, minkowski=False):
+
+    box = (cube([0,0,0]))
+
+    if(minkowski):
+        box = (Create_Round_Box(dimensions,wall))
+    else:
+        box = (cube([dimensions]))
+
+    return box
+
+def Create_Inserts(dimensions, num_of_inserts, minkowski=False):
+
+    x,y,z = dimensions
+
+    insert = (cube([0,0,0]))
+    return insert;
+
+def Create_Round_Box(size, radius, shape='cylinder'):
+    """"box with rounded edges."""
+
+    valid_shapes = {'cylinder','sphere'}
+    if shape not in valid_shapes:
+        raise ValueError("Invalid sim type. Expected one of: %s" % valid_shapes)
+
+    x,y,z = size
+    x = x - radius * 2
+    y = y - radius * 2
+    z = z - 1
+
+    round_box = (cube([x,y,z]))
+    if shape == 'cylinder':
+        round_box = minkowski()(round_box)(cylinder(r=radius))
+    if shape == 'sphere':
+        round_box = minkowski()(round_box)(sphere(r=radius))
+    round_box = translate([radius,radius,0])(round_box)
+
+    return round_box
