efa3c967b86a80ed2bfaa9101992c3e53db41244
chrisg.local@gmail.com <chrisg.local@gmail.com>
2022-09-09 13:23:03 +1000

    Box Template: Rewrite Create_Inserts function This function creates the number of inserts specified by insert_rows and insert_columns parameters. Optional parameters to set gap width and rounding shape.

--- Templates/Box_Template.py
+++ Templates/Box_Template.py
@@ -21,21 +21,38 @@ def Create_Box(dimensions, minkowski=False):
 
     return box
 
-def Create_Inserts(dimensions, num_of_inserts, minkowski=False):
+def Create_Inserts(dimensions, radius, insert_rows=1, insert_columns=1,
+                   row_gap=4, column_gap=4, height_gap=4, shape='cylinder'):
+
+    valid_shapes = {'cylinder','sphere'}
+    if shape not in valid_shapes:
+        raise ValueError("Invalid sim type. Expected one of: %s" % valid_shapes)
 
     x,y,z = dimensions
 
-    insert = (cube([0,0,0]))
-    return insert;
+    insert_x = (x - ((insert_rows + 1) * row_gap)) / insert_rows
+    insert_y = (y - (insert_columns + 1) * column_gap) / insert_columns
+    insert_z = z
+    insert_dimensions = [insert_x, insert_y, insert_z]
 
-def Create_Round_Box(size, radius, shape='cylinder'):
+    _template = Create_Round_Box(insert_dimensions, radius)
+    inserts = (cube([0,0,0]))
+    for row in range(insert_rows):
+        for column in range(insert_columns):
+            offset = [row * (row_gap + insert_x), column * (column_gap + insert_y), 0]
+            inserts += translate(offset)(_template)
+
+    inserts = translate([row_gap, column_gap, height_gap])(inserts)
+    return inserts;
+
+def Create_Round_Box(dimensions, radius, shape='cylinder'):
     """"box with rounded edges."""
 
     valid_shapes = {'cylinder','sphere'}
     if shape not in valid_shapes:
         raise ValueError("Invalid sim type. Expected one of: %s" % valid_shapes)
 
-    x,y,z = size
+    x,y,z = dimensions
     x = x - radius * 2
     y = y - radius * 2
     z = z - 1 if shape == 'cylinder' else z - radius * 2
